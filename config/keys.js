f816af49b7d5b7df83441667bb381c7589c393c2
salitordjman <salitorgi@gmail.com>
2022-01-21 00:52:20 +0200

    keys

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+//figures out what set credentials to return
+module.exports = require("./prod");
+
+
+
+// if (process.env.NODE_ENV === "production") {
+//   //production
+//   module.exports = require("./prod");
+// } else {
+//   //in development
+//   module.exports = require("./dev");
+// }
--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-
+const keys = require("../config/keys");
 mongoose.connect(
-  "mongodb+srv://salitor:123456Aaa@salitur.xfafu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  `mongodb+srv://salitor:${keys.password}@salitur.xfafu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   {}
 );
 // mongoose.connect("mongodb://127.0.0.1:27017/bank-api-mongoose-react-w-p", {
