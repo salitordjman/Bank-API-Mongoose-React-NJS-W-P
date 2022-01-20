856c99d1d36feb7d8251778b87dd5a2b720409f3
salitordjman <salitorgi@gmail.com>
2022-01-20 19:22:28 +0200

    connection  to postman, heroku, react

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
-// import { useState } from 'react';
-import myApi from './api/Api';
+import { useState } from "react";
+import myApi from "./api/Api";
 
 function App() {
-  // const [user, setUser] = useState('');
+  const [user, setUser] = useState("");
 
   console.log(process.env.NODE_ENV);
 
   const getReq = async () => {
-    const { data } = await myApi.get('/users');
+    const { data } = await myApi.get("/users");
     console.log(data);
   };
   return (
-    <div className='App'>
-      {' '}
+    <div className="App">
+      {" "}
       Hello World!
       <button onClick={getReq}>get</button>
     </div>
