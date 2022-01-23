0ac73e458840f37639bfc02a49c0080758973c87
salitordjman <salitorgi@gmail.com>
2022-01-23 04:23:21 +0200

    front

--- models/userDetails.js
+++ models/userDetails.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
-// const detailsSchema = mongoose.Schema({
-//   DateAdded: {
-//     type: Date,
-//     default: Date(),
-//   },
-//   isActive: {
-//     type: Boolean,
-//     default: true,
-//   },
-// });
+const detailsSchema = mongoose.Schema({
+  DateAdded: {
+    type: Date,
+    default: Date(),
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+});
 const userDetails = mongoose.model("userDetails", {
   name: {
     type: String,
@@ -34,7 +34,7 @@ const userDetails = mongoose.model("userDetails", {
     minLength: 7,
     maxLength: 10,
   },
-  // details: detailsSchema,
+  details: detailsSchema,
 });
 
 module.exports = userDetails;
